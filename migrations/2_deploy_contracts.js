2706fe5711d2c807272662107c7dcedca5cd6b4b
Cale Teeter <caleteeter@hotmail.com>
2018-10-09 01:28:35 -0400

    Update README.md for garbled text and migration with required parameters

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var RoomThermostat = artifacts.require('RoomThermostat');
 
-module.exports = (deployer) => {
-    deployer.deploy(RoomThermostat);
+module.exports = (deployer, network, accounts) => {
+    deployer.deploy(RoomThermostat, accounts[0], accounts[1]);
 }
